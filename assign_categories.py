273157c3658cd2458302ce825499127061dacf56
carichey93 <30934305+carichey93@users.noreply.github.com>
2022-12-08 09:52:18 -0600

    Added doc string

--- assign_categories.py
+++ assign_categories.py
@@ -1,4 +1,8 @@
 def add_plant_area(df, input_column, category_dict):
+    """ 
+    Creates a new column called "Category" in the dataframe and each rows value
+    is based on matched values from a dictionary of lists.
+    """
     for category, category_items in category_dict.items():
         df.loc[df[input_column].isin(category_list), "Category"] = category
 
